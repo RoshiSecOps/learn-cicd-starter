6234b2bd6364c074a2bf20b01d9044d827528bbd
Ivan Popov <116061712+RoshiSecOps@users.noreply.github.com>
2026-05-08 01:20:14 -0700

    Add linting via staticcheck.

--- internal/auth/get_api_key_test.go
+++ internal/auth/get_api_key_test.go
@@ -8,12 +8,12 @@ import (
 func TestCorrectGetAPIKEY(t *testing.T) {
 	headers := http.Header{}
 	headers.Add("Authorization", "ApiKey some-secret-key")
-	key, err := GetAPIKey(headers)
+	key, _ := GetAPIKey(headers)
 	if key != "some-secret-key" {
 		t.Fatalf("Expected %v, got %v", "some-secret-key", key)
 	}
 	newHeaders := http.Header{}
-	_, err = GetAPIKey(newHeaders)
+	_, err := GetAPIKey(newHeaders)
 	if err == nil {
 		t.Fatalf("Expected %v, got %v", "no authorization header included", err)
 	}
